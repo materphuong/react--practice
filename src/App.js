e35f8f98574d7f20f0272088fd83c15e77031980
phuong <materphuong@gmail.com>
2024-03-12 00:41:07 +0700

    update addNewUsers

--- src/App.js
+++ src/App.js
@@ -2,14 +2,33 @@ import { Container } from "react-bootstrap";
 import "./App.scss";
 import Header from "./components/header";
 import TableUsers from "./components/TableUsers";
+import ModalAddNew from "./components/ModalAddNew";
+import { useState } from "react";
 
 function App() {
+  const [isShowModalAddNew, setIsShowModalAddNew] = useState(false);
+  const handleClose = () => {
+    setIsShowModalAddNew(false);
+  };
   return (
     <div className="app-container">
       <Header></Header>
       <Container>
+        <div className="my-3 add-new">
+          <span>
+            {" "}
+            <b>List user:</b>{" "}
+          </span>
+          <button
+            class="btn btn-success"
+            onClick={() => setIsShowModalAddNew(true)}
+          >
+            Add new user
+          </button>
+        </div>
         <TableUsers></TableUsers>
       </Container>
+      <ModalAddNew show={isShowModalAddNew} handleClose={handleClose} />
     </div>
   );
 }
